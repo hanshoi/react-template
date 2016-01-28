42197e7cc504ae7b3aec303f0a0be947a026df77
Antti Ruotsalainen <antti.ruotsalainen@vuoma.fi>
2016-01-28 09:23:49 +0200

    added comments to files for easier interpreting

--- examples/helloworld/main.js
+++ examples/helloworld/main.js
@@ -1,7 +1,8 @@
-// main.js
+// require React components used
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+// render a h1 tag into html element with id="example"
 ReactDOM.render(
   <h1>Hello, world!</h1>,
   document.getElementById('example')
