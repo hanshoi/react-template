6024fd50a184e2160c8c8e5f8763fddd45a5792b
Antti Ruotsalainen <antti.ruotsalainen@vuoma.fi>
2016-02-02 15:22:16 +0200

    added some SASS stuff into this example

--- examples/bower/gulpfile.js
+++ examples/bower/gulpfile.js
@@ -4,13 +4,16 @@ var source = require('vinyl-source-stream');
 var browserify = require('browserify');
 var watchify = require('watchify');
 var reactify = require('reactify');
-//var notify = require('gulp-notify');
+var notify = require('gulp-notify');
+var sass = require('gulp-sass');
 var size = require('gulp-size');
 
 var path = {
   OUT: 'build.js',
-  DEST: 'application/static/dist',
-  ENTRYPOINT: './application/static/js/app.js'
+  DEST: 'application/static/scripts/js/',
+  ENTRYPOINT: './application/static/scripts/jsx/app.js',
+  SCSS: './application/static/stylesheets/scss/style.scss',
+  CSS: './application/static/stylesheets/css/style.css'
 };
 
 // create a watcher for .js files by wrapping browserify with watchify
@@ -34,8 +37,20 @@ function handleErrors() {
   this.emit('end'); // Keep gulp from hanging on this task
 }
 
+
+// sass compiling
+gulp.task('styles', function() {
+  gulp.src(path.SCSS)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest(path.CSS))
+    .pipe(size());
+});
+
+
 // main development task
 gulp.task('watch', function() {
+  gulp.watch(path.SCSS, ['styles']);
+
   var watcher = getWatcher();
 
   function rebundle() {
@@ -51,4 +66,4 @@ gulp.task('watch', function() {
   return watcher.on('update', rebundle);
 });
 
-gulp.task('default', ['watch']);
+gulp.task('default', ['styles', 'watch']);
