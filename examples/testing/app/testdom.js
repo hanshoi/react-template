594751cb3c29c1483ef38b259c2a40829c1399c7
Antti Ruotsalainen <antti.ruotsalainen@vuoma.fi>
2016-02-06 16:36:48 +0200

    cleaned out the unnecessary parts

--- examples/testing/app/testdom.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(markup) {
-  if (typeof document !== 'undefined') return;
-  var jsdom = require('jsdom').jsdom;
-  global.document = jsdom(markup || '<html><body></body></html>');
-  global.window = document.parentWindow;
-  global.navigator = {
-    userAgent: 'node.js'
-  };
-};
--- examples/testing/gulpfile.js
+++ examples/testing/gulpfile.js
@@ -4,27 +4,18 @@ var _ = require('underscore');
 var source = require('vinyl-source-stream');
 var browserify = require('browserify');
 var watchify = require('watchify');
-var reactify = require('reactify');
+var babelify = require('babelify');
 var notify = require('gulp-notify');
 var size = require('gulp-size');
 var livereload = require('gulp-livereload');
-var karmaServer = require('karma').Server;
 var conf = require('./package.json');
 
-/*
-var path = {
-  OUT: 'build.js',
-  DEST: 'app',
-  ENTRYPOINT: './app/app.js',
-  KARMA_CONF: '/karma.conf.js'
-};
-*/
 
 // create a watcher for .js files by wrapping browserify with watchify
 function getBundler(entrypoint, external) {
   return watchify(browserify({
     entries: [entrypoint],
-    transform: [reactify],
+    transform: [babelify],
     debug: conf.vars.develop,
     bundleExternal: external,    // prevent bundling vendor packages (react, react-dom)
     cache: {}, packageCache: {}, fullPaths: true
@@ -59,40 +50,6 @@ gulp.task('watch', function() {
 
   rebundleApp();
   app_bundler.on('update', rebundleApp);
-
-  // TESTS
-  var test_bundler = getBundler(conf.vars.test_files, true);
-
-  function rebundleTests() {
-    test_bundler.bundle()
-      .on('error', gutil.log)
-      .pipe(source(conf.vars.test_bundle))
-      .pipe(gulp.dest(conf.vars.build_folder))
-      .pipe(size());
-    gutil.log("Rebundle tests...");
-  }
-
-  rebundleTests();
-  test_bundler.on('update', rebundleTests);
-
-});
-
-
-// run test once and exit (equilavent of 'karma start')
-gulp.task('test', function (done) {
-  new karmaServer({
-    configFile: __dirname + conf.vars.karma_file,
-    singleRun: true
-  }, done()).start();
-});
-
-
-// run test once and exit
-gulp.task('tdd', function (done) {
-  new karmaServer({
-    configFile: __dirname + conf.vars.karma_file,
-    singleRun: false
-  }, done()).start();
 });
 
 
