649bb8fd7b46c089b6e167408463080835beec94
Antti Ruotsalainen <antti.ruotsalainen@vuoma.fi>
2016-02-10 08:37:27 +0200

    did some major streamlining for this application excample to show a very simple example

--- examples/isomorphic/client/components/app.js
+++ examples/isomorphic/client/components/app.js
@@ -1,36 +1,12 @@
 import React from "react";
-import DynamicSearch from "./dynamicsearch";
-import CountryEditor from "./countryeditor";
-import CountryStore from "../stores/countrystore";
 
 
-
-class App extends React.Component{
-  constructor() {
-    super();
-    this.state = CountryStore.getState();
-    this.storeChanged = (state) => {
-      this.setState(state);
-    };
-  }
-
-  componentDidMount() {
-    CountryStore.listen(this.storeChanged);
-  }
-
-  componentWillUnmount() {
-    CountryStore.unlisten(this.storeChanged);
-  }
-
+export default class App extends React.Component{
   render() {
-    let countries = this.state.countries;
     return (
       <div>
-        <DynamicSearch countries={countries} />
-        <CountryEditor />
+        <h1>Really nice component</h1>
       </div>
     );
   }
 }
-
-export default App;
--- examples/isomorphic/server/index.js
+++ examples/isomorphic/server/index.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import React from  "react";
 import ReactDOMServer from "react-dom/server";
-import App from "";
+import App from "./generated/server.build";
  
 const app = express();
  
 app.get('/', (request, response) => {
-  response.send('Hello world from Express');
+  response.send(ReactDOMServer.renderToString(<App />));
 });
  
 app.listen(3000, () => console.log('Server running on http://localhost:3000'));
