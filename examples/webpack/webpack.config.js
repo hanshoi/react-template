d5a087095feb2255606af680dec95876cc822c16
Antti Ruotsalainen <antti.ruotsalainen@vuoma.fi>
2016-02-22 11:18:27 +0200

    added some necessary webpack files

---
+++ examples/webpack/webpack.config.js
@@ -0,0 +1,47 @@
+var PATHS = {
+  app: "./frontend/javascript/app.js",
+  build: "./backend/static/"
+};
+
+module.exports = {
+  entry: {
+    app: PATHS.app
+  },
+
+  // Add resolve.extensions.
+  // '' is needed to allow imports without an extension.
+  // Note the .'s before extensions as it will fail to match without!!!
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+
+  output: {
+    path: PATHS.build,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.scss$/,
+        // for sourcemaps you need to pass css?sourceMaps and sass?sourceMaps to loaders
+        loaders: ['style', 'css', 'sass'],
+        include: PATHS.app
+      }
+
+      },
+      // Set up jsx. This accepts js too thanks to RegExp
+      {
+        test: /\.jsx?$/,
+        // Enable caching for improved performance during development
+        loaders: ['babel?cacheDirectory'],
+        // Parse only app files! Without this it will go through entire project.
+        // In addition to being slow, that will most likely result in an error.
+        include: PATHS.app
+        query: {
+          cacheDirectory: true,
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  }
+};
